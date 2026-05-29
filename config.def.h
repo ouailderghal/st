b020df5326d7190b263e9ce33118801297c51efd
Ouail Derghal <ouailderghal@protonmail.com>
2026-05-29 16:06:28 +0200

    add font2 patch

--- config.def.h
+++ config.def.h
@@ -6,6 +6,15 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Hack Nerd Font Mono:pixelsize=14:antialias=true:autohint=true";
+
+static char *font2[] = {
+/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
+/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+	"Noto Color Emoji:pixelsize=14:antialias=true:autohint=true",
+	"Noto Sans Mono CJK JP:pixelsize=14:antialias=true:autohint=true",
+	"Noto Sans:pixelsize=14:antialias=true:autohint=true",
+};
+
 static int borderpx = 2;
 
 /*
